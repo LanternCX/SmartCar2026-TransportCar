68c55cd4cb767590ca0a11403eff5dc4137bc4d9
CaoXin <caoxin@xysu.tech>
2025-12-11 16:00:25 +0800

    feat: three-motor filter

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import gc
 
 # 配置项
 TICK_MS = 5
-MAX_DUTY = 10000
+MAX_DUTY = 5000
 DUTY_STEP = 10
 
 # UART 调试
@@ -15,57 +15,93 @@ uart3 = UART(2)
 uart3.init(115200)
 
 # LED 与拨码开关
-led = Pin('C4', Pin.OUT, value=True)
-switch2 = Pin('D9', Pin.IN, pull=Pin.PULL_UP_47K)
+led = Pin("C4", Pin.OUT, value=True)
+switch2 = Pin("D9", Pin.IN, pull=Pin.PULL_UP_47K)
 state2 = switch2.value()
 
 # 编码器与 PWM
-encoder_1 = encoder("D15", "D16", True)
-motor_1 = MOTOR_CONTROLLER(MOTOR_CONTROLLER.PWM_C30_DIR_C31, 13000, duty=0, invert=False)
+encoder_m = encoder("D15", "D16", True)
+encoder_l = encoder("C0", "C1", True)
+encoder_r = encoder("C2", "C3", True)
+
+motor_m = MOTOR_CONTROLLER(
+    MOTOR_CONTROLLER.PWM_C30_DIR_C31, 13000, duty=0, invert=False
+)
+motor_l = MOTOR_CONTROLLER(MOTOR_CONTROLLER.PWM_D6_DIR_D7, 13000, duty=0, invert=True)
+motor_r = MOTOR_CONTROLLER(MOTOR_CONTROLLER.PWM_D4_DIR_D5, 13000, duty=0, invert=False)
+
+
+def build_wheel_state(name, encoder_obj, motor_obj):
+    return {
+        "name": name,
+        "encoder": encoder_obj,
+        "motor": motor_obj,
+        "dual_filter": DualWindowRegressionFilter(
+            tick_ms=TICK_MS,
+            long_window=30,
+            short_window=8,
+            combine_w=0.65,
+        ),
+        "input_lpf": LowPassFilter(alpha=0.5),
+        "output_lpf": LowPassFilter(alpha=0.5),
+        "raw_speed": 0.0,
+        "filtered_speed": 0.0,
+    }
+
+
+wheel_states = [
+    build_wheel_state("m", encoder_m, motor_m),
+    build_wheel_state("l", encoder_l, motor_l),
+    build_wheel_state("r", encoder_r, motor_r),
+]
+all_motors = [state["motor"] for state in wheel_states]
 
 # 采样 ticker
 pit_flag = False
+
+
 def pit_handler(tick):
     global pit_flag
     pit_flag = True
 
+
 pit1 = ticker(1)
-pit1.capture_list(encoder_1)
+pit1.capture_list(*[state["encoder"] for state in wheel_states])
 pit1.callback(pit_handler)
 pit1.start(TICK_MS)
 
-# 滤波器
-dual_filter = DualWindowRegressionFilter(
-    tick_ms=TICK_MS,
-    long_window=30,
-    short_window=8,
-    combine_w=0.65
-)
-
-# 输出低通滤波器（对回归融合后的速度做低通）
-output_lpf = LowPassFilter(alpha=0.5)
-input_lpf = LowPassFilter(alpha=0.5)
-
+# 滤波器负责在 wheel_states 中单独构造
 now_duty = 0
 
 while True:
     if pit_flag:
         led.toggle()
-        raw_speed = float(encoder_1.get())
-        smooth_raw_speed = input_lpf.update(raw_speed)
-        fused_speed, accel, _ = dual_filter.update(smooth_raw_speed)
-        smooth_speed = output_lpf.update(fused_speed)
-        uart3.write("{:.2f},{:.2f},{:.2f}\r\n".format(smooth_speed, raw_speed, accel))
+        for state in wheel_states:
+            raw = float(state["encoder"].get())
+            state["raw_speed"] = raw
+            smooth_raw = state["input_lpf"].update(raw)
+            fused_speed, _, _ = state["dual_filter"].update(smooth_raw)
+            state["filtered_speed"] = state["output_lpf"].update(fused_speed)
+        sample = ",".join(
+            "{:.2f}".format(v)
+            for state in wheel_states
+            for v in (state["raw_speed"], state["filtered_speed"])
+        )
+        uart3.write(sample + "\r\n")
         pit_flag = False
 
-        motor_1.duty(now_duty)
-        now_duty += DUTY_STEP
-        if now_duty > MAX_DUTY:
-            now_duty = MAX_DUTY
+        # for i in range(len(all_motors)):
+        #     if i == 1:
+        #         continue
+        #     all_motors[i].duty(now_duty)
+        for motor in all_motors:
+            motor.duty(now_duty)
+        now_duty = min(now_duty + DUTY_STEP, MAX_DUTY)
 
     if switch2.value() != state2:
         pit1.stop()
-        motor_1.duty(0)
+        for motor in all_motors:
+            motor.duty(0)
         uart3.write("stop\r\n")
         break
 
